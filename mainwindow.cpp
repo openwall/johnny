f58693d0ea20cb0efbd9a3c2bb4036bc693563e9
Aleksey Cherepanov <aleksey.4erepanov@gmail.com>
2011-07-17 23:38:15 +0400

    deleted old comment
    When list items was moved to .ui file this comment became useless.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,7 +13,6 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    // Create listview items
     connect(ui->listWidgetTabs, SIGNAL(itemSelectionChanged()),
             this, SLOT(selectPage()));
 

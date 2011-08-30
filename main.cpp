159b0ad92ff2775a9f8491435d872a89bd4c8937
Aleksey Cherepanov <aleksey.4erepanov@gmail.com>
2011-08-30 15:25:39 +0400

    code for settings page is added

--- main.cpp
+++ main.cpp
@@ -7,6 +7,14 @@
 
 int main(int argc, char *argv[])
 {
+    // We set application info up.
+    // It is needed to be able to store settings easily.
+    // TODO: Is this info right?
+    // TODO: Is that the right place for this?
+    QCoreApplication::setOrganizationName("OpenWall");
+    QCoreApplication::setOrganizationDomain("OpenWall.com");
+    QCoreApplication::setApplicationName("Johnny, the GUI for John the Ripper");
+
     QApplication app(argc, argv);
     MainWindow window;
     window.show();

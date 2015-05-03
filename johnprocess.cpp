66d9f6e6cc1237843f3053b06d6840fbc7572612
Mathieu Laprise <mathieu.laprise@polymtl.ca>
2015-05-03 14:03:38 -0400

    Add comments regarding -- fork fix from last commit

--- johnprocess.cpp
+++ johnprocess.cpp
@@ -14,6 +14,8 @@ JohnProcess::~JohnProcess()
 void JohnProcess::setupChildProcess()
 {
 #ifdef OS_FORK
+    //Create a group for children created by
+    // JtR when -- fork option is available (unix-like)
     setsid();
 #endif
 
@@ -25,6 +27,11 @@ void JohnProcess::terminate()
 #ifdef OS_FORK
     if(processId() != 0)
     {
+        /* Send sigterm to all processes of the group
+         * created previously with setsid()
+         * This is done in Johnny because of a bug in john <= 1.8.0
+         * where it didn't forward signals to children and we want to keep
+         * compatibility with those versions of John for now */
         ::kill(-processId(),SIGTERM);
     }
 #endif
